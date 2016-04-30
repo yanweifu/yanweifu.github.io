c785e8b3b859c0db71d7933f255148c29099bf96
yanweifu <fuylocal@elliott.drp.cs.cmu.edu>
2016-04-30 13:21:00 -0400

    adding more style files
    adding style files

---
+++ branding.js
@@ -0,0 +1,7 @@
+function ucbheader(){
+	document.write('<img src="" border="0" usemap="#QMUL" /><map name="UCBMap" id="UCBMap"><area shape="rect" coords="11,3,298,27" href="http://vision.eecs.qmul.ac.uk/" alt="http://vision.eecs.qmul.ac.uk/" target="_blank" /><area shape="rect" coords="794,7,828,25" href="http://http://vision.eecs.qmul.ac.uk/" alt="Vision Home" target="_blank" /><area shape="rect" coords="835,7,873,25" href="http://www.http://vision.eecs.qmul.ac.uk/" alt="Vision Search" target="_blank"  /><area shape="rect" coords="880,7,916,25" href="http://www.eecs.qmul.ac.uk/atoz/" alt="" target="_blank"  /><area shape="rect" coords="925,7,949,25" href="http://vision.eecs.qmul.ac.uk/" alt="Campus Map" target="_blank" /></map>');
+}
+
+function ucbfooter(){
+	document.write('<a target="_blank" href="http://www.eecs.qmul.ac.uk/"><img src="" alt="EECS, Queen Mary University of London" /></a>');	
+}
